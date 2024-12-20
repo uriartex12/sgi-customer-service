2e44599719b28aebe92aea7aac76342794f2610b
uriartex12 <prietomatex_1998@hotmail.com>
2024-12-19 23:13:42 -0500

    first commit

---
+++ src/main/java/com/sgi/sgi_customer_back/SgiCustomerBackApplication.java
@@ -0,0 +1,13 @@
+package com.sgi.sgi_customer_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgiCustomerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgiCustomerBackApplication.class, args);
+	}
+
+}
